b7f666e83746abc6d57f671994f7f22ab9a65287
Gabriel Felipe Huwe <gabrielhuwe@gmail.com>
2020-07-24 13:42:13 -0300

    Integraçao com Firebase Auth

--- src/components/app/app-login/app-login.tsx
+++ src/components/app/app-login/app-login.tsx
@@ -1,10 +1,21 @@
-import { Component, h } from '@stencil/core';
+import { Component, h } from "@stencil/core";
+import { AuthService } from "../../../helpers/auth";
 
 @Component({
-  tag: 'app-login',
-  styleUrl: 'app-login.css'
+  tag: "app-login",
+  styleUrl: "app-login.css",
 })
 export class AppLogin {
+  async loginWithGoogle() {
+    const user = await AuthService.loginWithGoogle();
+    console.log(user);
+  }
+
+  async loginWithFacebook() {
+    const user = await AuthService.loginWithFacebook();
+    console.log(user);
+  }
+
   render() {
     return [
       <ion-grid>
@@ -17,44 +28,60 @@ export class AppLogin {
           <ion-col size="12">
             <ion-item>
               <ion-label position="floating">E-mail:</ion-label>
-              <ion-input
-                type="email"
-                name="email"
-              ></ion-input>
+              <ion-input type="email" name="email"></ion-input>
             </ion-item>
           </ion-col>
           <ion-col size="12">
             <ion-item>
               <ion-label position="floating">Senha:</ion-label>
-              <ion-input
-                type="password"
-                name="password"
-              ></ion-input>
+              <ion-input type="password" name="password"></ion-input>
             </ion-item>
           </ion-col>
         </ion-row>
         <ion-row>
           <ion-col size="6">
-            <ion-button href="#" color="primary" expand="block">Entrar</ion-button>
+            <ion-button href="#" color="primary" expand="block">
+              Entrar
+            </ion-button>
           </ion-col>
           <ion-col size="6">
-            <ion-button href="/register" color="medium" expand="block">Criar conta</ion-button>
+            <ion-button href="/register" color="medium" expand="block">
+              Criar conta
+            </ion-button>
           </ion-col>
-          <ion-col size="12" class="ion-text-center"><small><a href="/passwordrecovery">Perdeu a sua senha?</a></small></ion-col>
+          <ion-col size="12" class="ion-text-center">
+            <small>
+              <a href="/passwordrecovery">Perdeu a sua senha?</a>
+            </small>
+          </ion-col>
         </ion-row>
-        <hr/>
+        <hr />
         <ion-row>
           <ion-col size="12" class="ion-text-center ion-text-uppercase">
             <p>Ou entre com a sua conta</p>
           </ion-col>
           <ion-col size="12">
-            <ion-button href="#" color="primary" expand="block">Facebook</ion-button>
+            <ion-button
+              href="#"
+              onClick={() => this.loginWithFacebook()}
+              color="primary"
+              expand="block"
+            >
+              Facebook
+            </ion-button>
           </ion-col>
           <ion-col size="12">
-            <ion-button href="#" color="bt-google" expand="block">Google</ion-button>
+            <ion-button
+              href="#"
+              onClick={() => this.loginWithGoogle()}
+              color="bt-google"
+              expand="block"
+            >
+              Google
+            </ion-button>
           </ion-col>
         </ion-row>
-      </ion-grid>
+      </ion-grid>,
     ];
   }
 }
---
+++ src/helpers/auth.ts
@@ -0,0 +1,71 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+
+class AuthController {
+  public user: firebase.User;
+
+  constructor() {
+    this.init();
+  }
+
+  init() {
+    const config = {
+      apiKey: "AIzaSyB1dqMPnYUD5PQ-p26k-2DlMqsXEYVX0g0",
+      authDomain: "sos-pollis.firebaseapp.com",
+      databaseURL: "https://sos-pollis.firebaseio.com",
+      projectId: "sos-pollis",
+      storageBucket: "sos-pollis.appspot.com",
+      messagingSenderId: "875001918975",
+      appId: "1:875001918975:web:481dcf01882e5f56d2e3ca",
+      measurementId: "G-M28L68DTVB",
+    };
+    firebase.initializeApp(config);
+
+    firebase.auth().onAuthStateChanged((user) => {
+      this.user = user;
+    });
+  }
+
+  async registerUser(email, pass): Promise<firebase.auth.UserCredential> {
+    try {
+      return await firebase.auth().createUserWithEmailAndPassword(email, pass);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async loginWithEmail(email, pass): Promise<firebase.auth.UserCredential> {
+    try {
+      return await firebase.auth().signInWithEmailAndPassword(email, pass);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async loginWithGoogle(): Promise<firebase.auth.UserCredential> {
+    try {
+      let provider: firebase.auth.GoogleAuthProvider = new firebase.auth.GoogleAuthProvider();
+      return await firebase.auth().signInWithPopup(provider);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async loginWithFacebook(): Promise<firebase.auth.UserCredential> {
+    try {
+      let provider: firebase.auth.FacebookAuthProvider = new firebase.auth.FacebookAuthProvider();
+      return await firebase.auth().signInWithPopup(provider);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async sendVerificationEmail(): Promise<void> {
+    try {
+      return await firebase.auth().currentUser.sendEmailVerification();
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
+export const AuthService = new AuthController();
